fc630292adb620b765fe81be5fe518a212ac52a5
Justus Luthy <jluthy@expansioncapitalgroup.com>
2017-02-04 12:13:45 -0600

    Initial check-in of app dir

---
+++ app/web/app.php
@@ -0,0 +1,22 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+
+$loader = require __DIR__.'/../app/autoload.php';
+
+$environment = getenv('SYMFONY_ENV');
+$debug = (bool)getenv('SYMFONY_DEBUG');
+
+if ($debug == true) {
+    Symfony\Component\Debug\Debug::enable();
+}
+
+$app = new MicroKernel($environment, $debug);
+
+$app->loadClassCache();
+
+$request = Request::createFromGlobals();
+$response = $app->handle($request);
+$response->send();
+
+$app->terminate($request, $response);
